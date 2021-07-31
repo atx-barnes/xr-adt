51437db572c387ef9acff71509867a5b153f5154
Barnes-Charles <barnes_charles@bah.com>
2021-07-31 15:32:02 -0500

    small improvements

--- Assets/Airbase.cs
+++ Assets/Airbase.cs
@@ -9,31 +9,31 @@ public class Airbase : MonoBehaviour
     public AircraftScriptableObject Aircraft;
     public MapRenderer MapRenderer;
     public float AltitudeLimit = 3000;
-    public float MapZoomMinLimit = 13.5f;
-    public float MapZoomMaxMimit = 18;
+    public float MapZoomMaxLimit = 13.5f;
+    public float MapZoomMinMimit = 18;
     public float AircraftModelMaxYPosition = 1;
     public float AircraftModelMinYPosition = 0.1f;
     private Transform aircraft;
 
     public void Start()
     {
-        aircraft = Instantiate(Aircraft.AircraftPrefab, new Vector3(0,AircraftModelMinYPosition,0), Quaternion.identity, MapRenderer.transform).transform;
-        aircraft.localPosition = new Vector3(0, AircraftModelMinYPosition, 0);
+        aircraft = Instantiate(Aircraft.AircraftPrefab, new Vector3(0 ,AircraftModelMinYPosition ,0), Quaternion.identity, MapRenderer.transform).transform;
+        aircraft.localPosition = new Vector3(0,AircraftModelMinYPosition , 0);
     }
 
     private void Update()
     {
-        MapRenderer.Center = new LatLon(Aircraft.Data.Latitude * Mathf.Rad2Deg, Aircraft.Data.Longitude * Mathf.Rad2Deg);
-        MapRenderer.ZoomLevel = (MapZoomMaxMimit - RemapValue(0, AltitudeLimit, MapZoomMinLimit, MapZoomMaxMimit, (float)Aircraft.Data.Altitude)) + MapZoomMinLimit;
-        aircraft.localRotation = Quaternion.Euler((float)Aircraft.Data.Pitch * Mathf.Rad2Deg, (float)Aircraft.Data.Heading * Mathf.Rad2Deg, (float)Aircraft.Data.Bank * Mathf.Rad2Deg);
+        MapRenderer.Center = new LatLon(Aircraft.Data.Latitude, Aircraft.Data.Longitude);
+        MapRenderer.ZoomLevel = MapZoomMinMimit - RemapValue(0, AltitudeLimit, MapZoomMaxLimit, MapZoomMinMimit, (float)Aircraft.Data.Altitude) + MapZoomMaxLimit;
+        aircraft.localRotation = Quaternion.Euler((float)Aircraft.Data.Pitch, (float)Aircraft.Data.Heading, (float)Aircraft.Data.Bank);
         aircraft.localPosition = new Vector3(0, RemapValue(0, AltitudeLimit, AircraftModelMinYPosition, AircraftModelMaxYPosition, (float)Aircraft.Data.Altitude), 0);
     }
 
-    public float RemapValue(float oldMin, float oldMax, float newMin, float newMax, float oldValue)
+    public float RemapValue(float oMin, float oMax, float nMin, float nMax, float oValue)
     {
-        float OldRange = (oldMax - oldMin);
-        float NewRange = (newMax - newMin);
-        float NewValue = (((oldValue - oldMin) * NewRange) / OldRange) + newMin;
-        return (Mathf.Clamp(NewValue, newMin, newMax));
+        float oRange = (oMax - oMin);
+        float nRange = (nMax - nMin);
+        float nValue = (((oValue - oMin) * nRange) / oRange) + nMin;
+        return (Mathf.Clamp(nValue, nMin, nMax));
     }
 }
--- Assets/Scripts/AircraftData.cs
+++ Assets/Scripts/AircraftData.cs
@@ -5,26 +5,34 @@ using UnityEngine;
 public class AircraftData
 {
     [field: SerializeField]
-    public string Id { get; set; }
+    private string _id;
+    public string Id { get => _id; set => _id = value; }
 
     [field: SerializeField]
-    public double Pitch { get; set; }
+    private double _pitch;
+    public double Pitch { get => _pitch; set => _pitch = value * Mathf.Rad2Deg; }
 
     [field: SerializeField]
-    public double Altitude { get; set; }
+    private double _altitude;
+    public double Altitude { get => _altitude; set => _altitude = value; }
 
     [field: SerializeField]
-    public double Heading { get; set; }
+    private double _heading;
+    public double Heading { get => _heading; set => _heading = value * Mathf.Rad2Deg; }
 
     [field: SerializeField]
-    public double Longitude { get; set; }
+    private double _longitude;
+    public double Longitude { get => _longitude; set => _longitude = value * Mathf.Rad2Deg; }
 
     [field: SerializeField]
-    public double Latitude { get; set; }
+    private double _latitude;
+    public double Latitude { get => _latitude; set => _latitude = value * Mathf.Rad2Deg; }
 
     [field: SerializeField]
-    public double Airspeed { get; set; }
+    private double _airspeed;
+    public double Airspeed { get => _airspeed; set => _airspeed = value; }
 
     [field: SerializeField]
-    public double Bank { get; set; }
+    private double _bank;
+    public double Bank { get => _bank; set => _bank = value * Mathf.Rad2Deg; }
 }
